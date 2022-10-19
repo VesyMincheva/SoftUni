73b1e458d630875d51fe44c9a34511fc2f9c5235
VesyMincheva <v.mincheva2013@gmail.com>
2022-10-19 18:54:53 +0300

    Upload exercises from Java Advanced course
    Upload exercises from Java Advanced course

---
+++ Java Advanced/ExamPrep_17102022/bakery/Bakery.java
@@ -0,0 +1,59 @@
+package ExamPrep_17102022.bakery;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class Bakery {
+    private String name;
+    private int capacity;
+    private List<Employee> employees;
+
+    public Bakery(String name, int capacity) {
+        this.name = name;
+        this.capacity = capacity;
+        employees = new ArrayList<>();
+    }
+
+    public void add (Employee employee){
+        if (capacity > employees.size()){
+            employees.add(employee);
+        }
+    }
+
+    public boolean remove (String name){
+        Employee employeeToRemove = getEmployee(name);
+        if (employeeToRemove != null){
+            employees.remove(employeeToRemove);
+            return true;
+        }
+        return false;
+    }
+
+    public Employee getOldestEmployee (){
+//
+        return employees.stream()
+                .max(Comparator.comparingInt(Employee::getAge))
+                .orElse(null);
+    }
+
+    public Employee getEmployee (String name){
+        return employees.stream().filter(e -> e.getName().equals(name))
+                .findFirst().orElse(null);
+    }
+
+    public int getCount (){
+        return employees.size();
+    }
+
+    public String report (){
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Employees working at Bakery %s:%n", name));
+        for (Employee employee : employees) {
+            sb.append(employee.toString());
+            sb.append(System.lineSeparator());
+        }
+        return sb.toString();
+    }
+
+}
