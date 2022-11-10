21e95a51647e560131b5abb996804841392f46b4
VesyMincheva <v.mincheva2013@gmail.com>
2022-11-10 16:39:28 +0200

    Upload exercises from Java OOP course
    Upload exercises from Java OOP course

---
+++ Java OOP/InheritanceExercises/vehicle/SportCar.java
@@ -0,0 +1,10 @@
+package vehicle;
+
+public class SportCar extends Car{
+    final static double DEFAULT_FUEL_CONSUMPTION = 10.00;
+
+    public SportCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+        setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
---
+++ Java OOP/InheritanceExercises/vehicle/Vehicle.java
@@ -0,0 +1,44 @@
+package vehicle;
+
+public class Vehicle {
+    public final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        setFuel(fuel);
+        setHorsePower(horsePower);
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+    }
+
+    public void drive (double kilometers) {
+        if (fuel >= (kilometers * fuelConsumption)){
+            fuel -= kilometers * fuelConsumption;
+        }
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+}
